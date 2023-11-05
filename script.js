7df0a6dd87d4493b88012ac9769aa504dabb91f9
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-11-05 23:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 //your code here!
-
+const containerHeight=document.getElementById("infi-list");
+console.log("scrollheight",containerHeight.scrollHeight);
+console.log("scrollheight",containerHeight.clientHeight);
